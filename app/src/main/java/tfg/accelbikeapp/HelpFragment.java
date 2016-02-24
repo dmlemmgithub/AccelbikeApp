693829b5afb8d1d9ca1a624fb89466f1d25e5f37
David Muñoz <david.lmd84@gmail.com>
2016-02-24 16:34:16 +0100

    Initial commit Estructura de pestañas deslizantes con menú lateral

---
+++ app/src/main/java/tfg/accelbikeapp/HelpFragment.java
@@ -0,0 +1,20 @@
+package tfg.accelbikeapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by David on 23/02/2016.
+ */
+public class HelpFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.help_layout,null);
+    }
+}
---
+++ app/src/main/java/tfg/accelbikeapp/PrincipalFragment.java
@@ -0,0 +1,19 @@
+package tfg.accelbikeapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/**
+ * Created by David on 22/02/2016.
+ */
+public class PrincipalFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.principal_layout,null);
+    }
+}

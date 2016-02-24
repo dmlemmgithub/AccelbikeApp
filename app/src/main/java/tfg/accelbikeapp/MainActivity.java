a3ea743a424ac0e2281adbf52579bfbcf4538311
alexis <mikeljulio@gmail.com>
2016-02-24 17:13:41 +0100

    Prueba :: comentario en mainActivity

--- app/src/main/java/tfg/accelbikeapp/MainActivity.java
+++ app/src/main/java/tfg/accelbikeapp/MainActivity.java
@@ -16,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
     FragmentManager mFragmentManager;
     FragmentTransaction mFragmentTransaction;
 
+    /**
+     * -------------------------------------------------------------
+     * -----------------Alexis--------------------------------------
+     */
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

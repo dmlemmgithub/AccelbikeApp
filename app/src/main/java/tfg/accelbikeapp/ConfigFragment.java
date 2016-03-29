6911797e02f1480392e41ca595ca34144fac06b3
alexis <mikeljulio@gmail.com>
2016-03-29 16:17:54 +0200

    29/03/2016 Alexis

--- app/src/main/java/tfg/accelbikeapp/ConfigFragment.java
+++ app/src/main/java/tfg/accelbikeapp/ConfigFragment.java
@@ -15,6 +15,6 @@ public class ConfigFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.config_layout,null);/**/
+        return inflater.inflate(R.layout.config_layout,null);
     }
 }
--- app/src/main/java/tfg/accelbikeapp/PrincipalFragment.java
+++ app/src/main/java/tfg/accelbikeapp/PrincipalFragment.java
@@ -6,23 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
-
+/**
+ * Created by David on 22/02/2016.
+ */
 public class PrincipalFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        View v = inflater.inflate(R.layout.principal_layout,container,false);
-        Button button = (Button)v.findViewById(R.id.accelerar);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity().getApplicationContext(), "Comprobando conexiones...",Toast.LENGTH_LONG).show();
-            }
-        });
-        return (v);
+        return inflater.inflate(R.layout.principal_layout,null);
     }
 }
